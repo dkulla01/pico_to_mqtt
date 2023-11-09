36bb42c5aadeebc77646cb96dbbcbd28e74ba9aa
Daniel Kulla <dkulla129@gmail.com>
2023-11-09 18:14:06 -0500

    start bringing in pico handling logic
    I also used pyright to create type stubs for some of the pylutron_caseta
    deps that show up in this project. We'll see how this goes

---
+++ src/pico_to_mqtt/caseta/topology.py
@@ -0,0 +1,71 @@
+import itertools
+import logging
+from asyncio import Condition
+
+from pylutron_caseta.smartbridge import Smartbridge
+
+from pico_to_mqtt.caseta.model import ButtonId, PicoThreeButtonRaiseLower
+from pico_to_mqtt.config import CasetaConfig
+
+LOGGER = logging.getLogger(__name__)
+
+
+def default_bridge(caseta_config: CasetaConfig) -> Smartbridge:
+    return Smartbridge.create_tls(
+        caseta_config.caseta_bridge_hostname,
+        caseta_config.path_to_caseta_client_key,
+        caseta_config.path_to_caseta_client_cert,
+        caseta_config.path_to_caseta_client_ca,
+    )
+
+
+class Topology:
+    def __init__(
+        self, caseta_bridge: Smartbridge, shutdown_condition: Condition
+    ) -> None:
+        self._caseta_bridge: Smartbridge = caseta_bridge
+        self._shutdown_condition = shutdown_condition
+        self._remotes_by_id: dict[int, PicoThreeButtonRaiseLower] = {}
+
+    async def connect(self) -> None:
+        LOGGER.info("connecting to caseta bridge")
+        await self._caseta_bridge.connect()
+
+        all_buttons = self._caseta_bridge.get_buttons()
+        all_devices = self._caseta_bridge.get_devices()
+        buttons_by_remote_id = {
+            remote_id: list(buttons)
+            for remote_id, buttons in itertools.groupby(
+                [button for button in all_buttons.values()],
+                lambda b: b["parent_device"],
+            )
+        }
+
+        for device_id, device in all_devices.items():
+            # skip devices that are not remotes
+            if device_id not in buttons_by_remote_id.keys():
+                continue
+
+            remote_buttons = buttons_by_remote_id[device["device_id"]]
+            buttons_by_id: dict[int, ButtonId] = {
+                int(button["device_id"]): ButtonId.of_int(button["button_number"])
+                for button in remote_buttons
+            }
+
+            device_name = device["name"].removesuffix("_Pico")
+            device_id_as_int = int(device_id)
+            if device["type"] == PicoThreeButtonRaiseLower.TYPE:
+                self._remotes_by_id[device_id_as_int] = PicoThreeButtonRaiseLower(
+                    device_id_as_int, device_name, buttons_by_id
+                )
+
+            else:
+                LOGGER.debug(
+                    (
+                        "device: %s: device type `%s` "
+                        "is not a supported pico remote and will be skipped"
+                    ),
+                    device["name"],
+                    device["type"],
+                )
+        LOGGER.info("done connecting to caseta bridge")
--- src/pico_to_mqtt/main.py
+++ src/pico_to_mqtt/main.py
@@ -1,4 +1,21 @@
-from pico_to_mqtt.config import get_config
+import asyncio
+import logging
+import os
+import sys
+from enum import Enum
+
+import attrs
+
+from pico_to_mqtt.caseta.topology import Topology, default_bridge
+from pico_to_mqtt.config import AllConfig, get_config
+
+_LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
+_HANDLER = logging.StreamHandler(stream=sys.stderr)
+_HANDLER.setLevel(_LOGLEVEL)
+_FORMATTER = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+_HANDLER.setFormatter(_FORMATTER)
+logging.basicConfig(level=_LOGLEVEL, handlers=[_HANDLER])
+LOGGER = logging.getLogger(__name__)
 
 
 def return_three_for_pytest_flow_check() -> int:
@@ -9,9 +26,31 @@ def print_hello_world() -> None:
     print("Hello, world! hello, Dan")
 
 
+async def consume_pico_messages(topology: Topology):
+    await topology.connect()
+    LOGGER.info("yay we've connected")
+
+
+async def main_loop(configuration: AllConfig):
+    shutdown_condition = asyncio.Condition()
+    topology = Topology(default_bridge(configuration.caseta_config), shutdown_condition)
+
+    await consume_pico_messages(topology)
+
 def main():
     configuration = get_config()
-    print(f"configuration: {configuration}")
+    logging.info(f"configuration: {configuration}")
+
+    loop = asyncio.new_event_loop()
+    try:
+        loop.create_task(main_loop(configuration))
+        loop.run_forever()
+    except KeyboardInterrupt:
+        logging.info("got a keyboard interrupt")
+    finally:
+        loop.close()
+
+    logging.info("we're done")
 
 
 if __name__ == "__main__":
